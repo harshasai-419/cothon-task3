ff5c1cf160408b0acad71ca11b09f1535ff44a1a
harshasai-419 <konkipudihsbhaskar419@gmail.com>
2025-01-27 19:41:46 +0530

    changed url

--- src/components/LoginForm/index.js
+++ src/components/LoginForm/index.js
@@ -37,7 +37,7 @@ class LoginForm extends Component {
     event.preventDefault()
     const {username, password} = this.state
     const userDetails = {username, password}
-    const url = 'https://apis.ccbp.in/login'
+    const url = 'https://cothon-task1-backend.onrender.com/login'
     const options = {
       method: 'POST',
       body: JSON.stringify(userDetails),
