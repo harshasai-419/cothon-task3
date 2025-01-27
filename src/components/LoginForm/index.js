dff73e78d0c5bc633bda5f6e4a1152bd9bb318b6
harshasai-419 <konkipudihsbhaskar419@gmail.com>
2025-01-27 20:01:13 +0530

    set to default

--- src/components/LoginForm/index.js
+++ src/components/LoginForm/index.js
@@ -37,7 +37,7 @@ class LoginForm extends Component {
     event.preventDefault()
     const {username, password} = this.state
     const userDetails = {username, password}
-    const url = 'https://cothon-task1-backend.onrender.com/login'
+    const url = 'https://apis.ccbp.in/login'
     const options = {
       method: 'POST',
       body: JSON.stringify(userDetails),
